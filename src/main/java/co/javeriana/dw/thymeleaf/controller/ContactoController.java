675b11a32cb2d00890182071350cc52e5c10382c
joseval29 <josecastiblanco2910@gmail.com>
2026-02-19 09:34:52 -0500

    Proyecto  con Docker funcionando

---
+++ src/main/java/co/javeriana/dw/thymeleaf/controller/ContactoController.java
@@ -0,0 +1,13 @@
+package co.javeriana.dw.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactoController {
+
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto";
+    }
+}
